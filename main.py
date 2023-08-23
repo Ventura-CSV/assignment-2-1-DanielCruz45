1f9b77739d73274b314c2d180162fe22c590196d
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-23 08:11:46 -0700

    format lined up, test time

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def main():
 
     #displays all values under the desired format and out puts them
     print(f'The total number of students: \t \t {total}')
-    print(f'The number of males and females: \t {male} \t {female}')
-    print(f'The percentage of males and females: \t{m_perc:.2f}% \t{f_perc:.2f}%')
+    print(f'The number of males and females: \t {male} \t \t {female}')
+    print(f'The percentage of males and females: \t {m_perc:.2f}% \t {f_perc:.2f}%')
 
     """
     ########################################
