67bf10a42afbd19f9e5a364c948e9ca46b3e7518
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-23 08:05:28 -0700

    formatting is here, missing %

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main():
     f_perc= (female / total) * 100
 
     #displays all values under the desired format and out puts them
-    print(total)
-    print({male}, {female})
-    print({m_perc}, {f_perc})
+    print(f'The total number of students: \t \t {total}')
+    print(f'The number of males and females: \t {male} \t {female}')
+    print(f'The percentage of males and females: \t {m_perc:.2f} \t {f_perc:.2f}')
 
     """
     ########################################
