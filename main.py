cb4193b3b0125bf0647367650dcedcebd5f05b94
DanielCruz45 <dcruz44@ucmerced.edu>
2023-08-23 08:10:06 -0700

    for mat is still a little off for %s

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def main():
     ##################################################
     """
     
-    #asks for input of genders in class
-    male = int(input ('male head count'))
-    female= int(input('female head count'))
+    #asks for input of genders in class, messages added for clarity for in reguards to the gender your giving a number to
+    male = int(input ('male count '))
+    female= int(input('female count '))
 
     #finds the class total through adding the genders input
     total= male + female
@@ -20,7 +20,7 @@ def main():
     #displays all values under the desired format and out puts them
     print(f'The total number of students: \t \t {total}')
     print(f'The number of males and females: \t {male} \t {female}')
-    print(f'The percentage of males and females: \t {m_perc:.2f} \t {f_perc:.2f}')
+    print(f'The percentage of males and females: \t{m_perc:.2f}% \t{f_perc:.2f}%')
 
     """
     ########################################
